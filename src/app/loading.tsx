a332291c36a75054f11bdb3d416de4a5e1961f0f
lordever <lordever@mail.ru>
2024-03-10 20:57:05 +0500

    Lesson 1.8

---
+++ src/app/loading.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, {FC} from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+
+const LoadingPage: FC<{ loading: boolean }> = ({loading}) => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100vh',
+            margin: '0 auto'
+        }}>
+            <ClipLoader
+                color="#3b82f6"
+                loading={loading}
+                size={150}
+                aria-label="Loading Spinner"
+            />
+        </div>
+    );
+};
+
+export default LoadingPage;
