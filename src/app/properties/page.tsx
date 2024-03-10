4319f94da9e396ea3ea328d3cf427f166ad15ff3
lordever <lordever@mail.ru>
2024-03-10 20:31:30 +0500

    Lesson 1.6

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/property-card/property.card.component";
+import {PropertyModel} from "@/models/property.model";
 
 const PropertiesPage = () => {
     return (
-        <div>
-            <h1 className="text-3xl">Properties</h1>
-            <Link href="/">Go Home</Link>
-        </div>
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                {!Array.isArray(properties) ? (
+                    <p>No properties found</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property: Partial<PropertyModel>) => (
+                            <PropertyCard key={property._id} property={property as PropertyModel}/>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
     );
 };
 
---
+++ src/models/property.model.ts
@@ -0,0 +1,20 @@
+export interface PropertyModel {
+    _id: string;
+    type: string;
+    name: string;
+    beds: number;
+    baths: number;
+    square_feet: number;
+    rates: {
+        monthly?: number;
+        weekly?: number;
+        nightly?: number;
+    }
+    location: {
+        city: string;
+        street: string;
+        state: string;
+        zipcode: string;
+    }
+    images: string[];
+}
