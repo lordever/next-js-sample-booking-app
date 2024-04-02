810eedc133641b2eb1c420ff18af07f5f3b5af24
lordever <lordever@mail.ru>
2024-04-03 00:54:11 +0500

    Lesson 7.4

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropertyCard from "@/components/property-card/property.card.component";
 import {PropertyModel} from "@/models/property.model";
 import {fetchProperties} from "@/services/property.service";
+import PropertySearchForm from "@/components/property-search-form/property-search-form.component";
+import Spinner from "@/components/spinner/spinner.component";
+import Link from "next/link";
+import {FaArrowAltCircleLeft} from "react-icons/fa";
 
 const PropertiesPage = async () => {
     const properties = await fetchProperties();
@@ -9,11 +13,17 @@ const PropertiesPage = async () => {
     //Sort properties by date
     properties.sort((a: PropertyModel, b: PropertyModel) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
 
-    return (
+    return <>
+        <section className="bg-blue-700 py-4">
+            <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+                <PropertySearchForm/>
+            </div>
+        </section>
+
         <section className="px-4 py-6">
             <div className="container-xl lg:container m-auto px-4 py-6">
                 {properties.length === 0 ? (
-                    <p>No properties found</p>
+                    <p>No search results found</p>
                 ) : (
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                         {properties.map((property: Partial<PropertyModel>) => (
@@ -23,7 +33,8 @@ const PropertiesPage = async () => {
                 )}
             </div>
         </section>
-    );
+    </>
+
 };
 
 export default PropertiesPage;
--- src/app/properties/search-results/page.tsx
+++ src/app/properties/search-results/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {PropertyModel} from "@/models/property.model";
 import {useSearchParams} from "next/navigation";
+import {FaArrowAltCircleLeft} from "react-icons/fa";
+import Spinner from "@/components/spinner/spinner.component";
+import PropertyCard from "@/components/property-card/property.card.component";
+import Link from "next/link";
+import PropertySearchForm from "@/components/property-search-form/property-search-form.component";
 
 
 const SearchResultsPage = () => {
@@ -34,9 +39,33 @@ const SearchResultsPage = () => {
         fetchPropertiesBySearchParams();
     }, [location, propertyType]);
 
-    console.log('Properties:', properties);
+    return <>
+        <section className="bg-blue-700 py-4">
+            <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+                <PropertySearchForm/>
+            </div>
+        </section>
 
-    return <div></div>;
+        {loading ? <Spinner loading={loading}/> : (
+            <section className="px-4 py-6">
+                <div className="container-xl lg:container m-auto px-4 py-6">
+                    <Link href="/properties" className="flex item-center text-blue-500 hover:underline mb-3">
+                        <FaArrowAltCircleLeft className="mr-2 mb-1"/> Back to properties
+                    </Link>
+                    <h1 className="text-2xl mb-4">Search results</h1>
+                    {properties.length === 0 ? (
+                        <p>No search results found</p>
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {properties.map((property: Partial<PropertyModel>) => (
+                                <PropertyCard key={property._id} property={property as PropertyModel}/>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </section>
+        )}
+    </>
 };
 
 export default SearchResultsPage;
