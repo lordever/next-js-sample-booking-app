e34b970baf8ada19a0e7265bc53d156b045835d6
lordever <lordever@mail.ru>
2024-04-09 00:13:08 +0500

    Lesson 8.5

--- src/components/messages/messages.component.tsx
+++ src/components/messages/messages.component.tsx
@@ -4,6 +4,8 @@ import React, {useEffect, useState} from 'react';
 import {MessageModel} from "@/models/message.model";
 import {fetchPropertyById} from "@/services/property.service";
 import {toast} from "react-toastify";
+import Message from "@/components/message/message.component";
+import Spinner from "@/components/spinner/spinner.component";
 
 const Messages = () => {
 
@@ -32,9 +34,27 @@ const Messages = () => {
     }, []);
 
     return (
-        <div>
+        <section className="bg-blue-50">
+            <div className="container m-auto py-24 max-w-6xl">
+                <div
+                    className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
+                >
+                    <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
 
-        </div>
+                    <div className="space-y-4">
+                        {loading ? <Spinner loading={loading}/> : (
+                            messages.length === 0 ? (
+                                <p>You have no messages</p>
+                            ) : (
+                                messages.map((message) => (
+                                    <Message key={message._id} message={message}/>
+                                ))
+                            )
+                        )}
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -1,10 +1,12 @@
 export interface MessageModel {
-    sender: { _id: string };
-    property: { _id: string };
+    _id: string;
     recipient: string;
     name: string;
     email: string;
     phone: string;
     body: string;
+    createdAt: string;
     read: boolean;
+    sender: { _id: string, username: string };
+    property: { _id: string, name: string };
 }
