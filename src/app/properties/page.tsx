462d61d741062e9e5a9f33b9cc667b9a9112c926
lordever <lordever@mail.ru>
2024-04-20 19:53:32 +0500

    Lesson 9.1

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import PropertyCard from "@/components/property-card/property.card.component";
-import {PropertyModel} from "@/models/property.model";
-import {fetchProperties} from "@/services/property.service";
 import PropertySearchForm from "@/components/property-search-form/property-search-form.component";
-import Spinner from "@/components/spinner/spinner.component";
-import Link from "next/link";
-import {FaArrowAltCircleLeft} from "react-icons/fa";
+import Properties from "@/components/properties/properties.component";
 
 const PropertiesPage = async () => {
-    const properties = await fetchProperties();
-
-    //Sort properties by date
-    properties.sort((a: PropertyModel, b: PropertyModel) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-
     return <>
         <section className="bg-blue-700 py-4">
             <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
@@ -20,19 +10,7 @@ const PropertiesPage = async () => {
             </div>
         </section>
 
-        <section className="px-4 py-6">
-            <div className="container-xl lg:container m-auto px-4 py-6">
-                {properties.length === 0 ? (
-                    <p>No search results found</p>
-                ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {properties.map((property: Partial<PropertyModel>) => (
-                            <PropertyCard key={property._id} property={property as PropertyModel}/>
-                        ))}
-                    </div>
-                )}
-            </div>
-        </section>
+        <Properties />
     </>
 
 };
---
+++ src/components/properties/properties.component.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React, {FC, useEffect, useState} from 'react';
+import {PropertyModel} from "@/models/property.model";
+import PropertyCard from "@/components/property-card/property.card.component";
+import {fetchProperties} from "@/services/property.service";
+import {toast} from "react-toastify";
+import Spinner from "@/components/spinner/spinner.component";
+
+const Properties: FC = async () => {
+    const [properties, setProperties] = useState<PropertyModel[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const processProperties = async () => {
+            try {
+                const properties = await fetchProperties();
+
+                //Sort properties by date
+                properties.sort((a: PropertyModel, b: PropertyModel) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+                setProperties(properties);
+            } catch (error) {
+                console.error(error);
+                toast.error("Getting properties was failed");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        processProperties();
+    }, []);
+
+    if (loading) {
+        return <Spinner loading={loading}/>
+    }
+
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                {properties.length === 0 ? (
+                    <p>No search results found</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property: Partial<PropertyModel>) => (
+                            <PropertyCard key={property._id} property={property as PropertyModel}/>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default Properties;
