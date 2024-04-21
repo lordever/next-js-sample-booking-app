a488507c60b0d87c321ac477643f782c72417bae
lordever <lordever@mail.ru>
2024-04-21 20:46:11 +0500

    Lesson 9.6

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth";
-import {authOptions} from "@/services/auth-options.service";
+import NextAuth from 'next-auth';
+import { authOptions } from '@/services/auth-options.service';
 
+// @ts-ignore
 const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
