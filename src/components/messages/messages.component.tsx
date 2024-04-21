b42ab29b4cc63cec2573086a1fbdd1cac1ba951c
lordever <lordever@mail.ru>
2024-04-21 19:33:55 +0500

    Lesson 9.2

--- src/components/messages/messages.component.tsx
+++ src/components/messages/messages.component.tsx
@@ -1,61 +1,56 @@
-"use client";
+'use client';
 
-import React, {useEffect, useState} from 'react';
-import {MessageModel} from "@/models/message.model";
-import {fetchPropertyById} from "@/services/property.service";
-import {toast} from "react-toastify";
-import Message from "@/components/message/message.component";
-import Spinner from "@/components/spinner/spinner.component";
+import React, { useEffect, useState } from 'react';
+import { MessageModel } from '@/models/message.model';
+import { toast } from 'react-toastify';
+import Message from '@/components/message/message.component';
+import Spinner from '@/components/spinner/spinner.component';
 
 const Messages = () => {
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    const [messages, setMessages] = useState<MessageModel[]>([]);
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await fetch('/api/messages');
 
+        if (res.status === 200) {
+          const data = await res.json();
+          setMessages(data);
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error('Fetching messages failed');
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        const getMessages = async () => {
-            try {
-                const res = await fetch("/api/messages");
+    getMessages();
+  }, []);
 
-                if (res.status === 200) {
-                    const data = await res.json();
-                    setMessages(data);
-                }
-            } catch (error) {
-                console.error(error);
-                toast.error("Fetching messages failed");
-            } finally {
-                setLoading(false);
-            }
-        };
+  return (
+    <section>
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
 
-        getMessages();
-    }, []);
-
-    return (
-        <section className="bg-blue-50">
-            <div className="container m-auto py-24 max-w-6xl">
-                <div
-                    className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
-                >
-                    <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
-
-                    <div className="space-y-4">
-                        {loading ? <Spinner loading={loading}/> : (
-                            messages.length === 0 ? (
-                                <p>You have no messages</p>
-                            ) : (
-                                messages.map((message) => (
-                                    <Message key={message._id} message={message}/>
-                                ))
-                            )
-                        )}
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          <div className="space-y-4">
+            {loading ? (
+              <Spinner loading={loading} />
+            ) : messages.length === 0 ? (
+              <p>You have no messages</p>
+            ) : (
+              messages.map(message => (
+                <Message key={message._id} message={message} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Messages;
--- src/components/properties/properties.component.tsx
+++ src/components/properties/properties.component.tsx
@@ -1,55 +1,72 @@
-"use client";
+'use client';
 
-import React, {FC, useEffect, useState} from 'react';
-import {PropertyModel} from "@/models/property.model";
-import PropertyCard from "@/components/property-card/property.card.component";
-import {fetchProperties} from "@/services/property.service";
-import {toast} from "react-toastify";
-import Spinner from "@/components/spinner/spinner.component";
+import React, { FC, useEffect, useState } from 'react';
+import { PropertyModel } from '@/models/property.model';
+import PropertyCard from '@/components/property-card/property.card.component';
+import { fetchProperties } from '@/services/property.service';
+import { toast } from 'react-toastify';
+import Spinner from '@/components/spinner/spinner.component';
+import Pagination from '@/components/pagination/pagination.component';
 
 const Properties: FC = () => {
-    const [properties, setProperties] = useState<PropertyModel[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(1);
-    const [pageSize, setPageSize] = useState(3);
-    const [totalItems, setTotalItems] = useState(0);
+  const [properties, setProperties] = useState<PropertyModel[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(3);
+  const [totalItems, setTotalItems] = useState(0);
 
-    useEffect(() => {
-        const processProperties = async () => {
-            try {
-                const data = await fetchProperties(page, pageSize);
-                setProperties(data.properties);
-                setTotalItems(data.totalCount);
-            } catch (error) {
-                console.error(error);
-                toast.error("Getting properties was failed");
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const processProperties = async () => {
+      try {
+        const data = await fetchProperties(page, pageSize);
+        setProperties(data.properties);
+        setTotalItems(data.totalCount);
+      } catch (error) {
+        console.error(error);
+        toast.error('Getting properties was failed');
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        processProperties();
-    }, []);
+    processProperties();
+  }, [page, pageSize]);
 
-    if (loading) {
-        return <Spinner loading={loading}/>
-    }
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
 
-    return (
-        <section className="px-4 py-6">
-            <div className="container-xl lg:container m-auto px-4 py-6">
-                {properties.length === 0 ? (
-                    <p>No search results found</p>
-                ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {properties.map((property: Partial<PropertyModel>) => (
-                            <PropertyCard key={property._id} property={property as PropertyModel}/>
-                        ))}
-                    </div>
-                )}
+  if (loading) {
+    return <Spinner loading={loading} />;
+  }
+
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No search results found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property: Partial<PropertyModel>) => (
+                <PropertyCard
+                  key={property._id}
+                  property={property as PropertyModel}
+                />
+              ))}
             </div>
-        </section>
-    );
+          )}
+        </div>
+      </section>
+
+      <Pagination
+        page={page}
+        pageSize={pageSize}
+        totalItems={totalItems}
+        onPageChange={handlePageChange}
+      />
+    </>
+  );
 };
 
 export default Properties;
