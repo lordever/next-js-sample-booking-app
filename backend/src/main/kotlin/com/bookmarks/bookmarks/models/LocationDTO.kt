94edf2f15507ee5fc558a8044f0672e403d991bd
lordever <lordever@mail.ru>
2024-08-11 00:06:42 +0500

    Mysql database dependencies

--- backend/src/main/kotlin/com/bookmarks/bookmarks/models/LocationDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.bookmarks.bookmarks.models
-
-data class LocationDTO(
-    var city: String? = null,
-    var street: String? = null,
-    var state: String? = null,
-    var zipcode: String? = null
-)
--- backend/src/main/kotlin/com/bookmarks/bookmarks/models/RatesDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.bookmarks.bookmarks.models
-
-data class RatesDTO(
-    var monthly: Int? = null,
-    var weekly: Int? = null,
-    var nightly: Int? = null
-)
